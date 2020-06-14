22bd1b5eda18ed3d5208155a66a28798658c4c56
BorislavNikolov <borislav_nikolov99@abv.bg>
2020-06-14 14:45:50 +0300

    Implement add article functionality. Improve design.

--- MMA_News_BG/Web/MMA_News_BG.Web/Controllers/CategoriesController.cs
+++ MMA_News_BG/Web/MMA_News_BG.Web/Controllers/CategoriesController.cs
@@ -1,52 +1,50 @@
 ﻿namespace MMA_News_BG.Web.Controllers
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Threading.Tasks;
 
     using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using MMA_News_BG.Services.Data;
 
     public class CategoriesController
     {
         private const int ItemsPerPage = 5;
 
         private readonly ICategoriesService categoriesService;
-        private readonly IPostsService postsService;
+        private readonly IArticlesService articlesService;
         private readonly IHttpContextAccessor http;
 
         public CategoriesController(
             ICategoriesService categoriesService,
-            IPostsService postsService,
+            IArticlesService articlesService,
             IHttpContextAccessor http)
         {
             this.categoriesService = categoriesService;
-            this.postsService = postsService;
+            this.articlesService = articlesService;
             this.http = http;
         }
 
-        public IActionResult ByName(string name, int page = 1)
-        {
-            var viewModel =
-                this.categoriesService.GetByName<CategoryViewModel>(name);
-            if (viewModel == null)
-            {
-                return this.NotFound();
-            }
+        //public IActionResult ByName(string name, int page = 1)
+        //{
+        //    var viewModel =
+        //        this.categoriesService.GetByName<CategoryViewModel>(name);
+        //    if (viewModel == null)
+        //    {
+        //        return this.NotFound();
+        //    }
 
-            viewModel.ForumPosts = this.postsService.GetByCategoryId<PostInCategoryViewModel>(viewModel.Id, ItemsPerPage, (page - 1) * ItemsPerPage);
+        //    viewModel.ForumPosts = this.articlesService.GetByCategoryId<PostInCategoryViewModel>(viewModel.Id, ItemsPerPage, (page - 1) * ItemsPerPage);
 
-            var count = this.postsService.GetCountByCategoryId(viewModel.Id);
-            viewModel.PagesCount = (int)Math.Ceiling((double)count / ItemsPerPage);
-            if (viewModel.PagesCount == 0)
-            {
-                viewModel.PagesCount = 1;
-            }
+        //    var count = this.articlesService.GetCountByCategoryId(viewModel.Id);
+        //    viewModel.PagesCount = (int)Math.Ceiling((double)count / ItemsPerPage);
+        //    if (viewModel.PagesCount == 0)
+        //    {
+        //        viewModel.PagesCount = 1;
+        //    }
 
-            viewModel.CurrentPage = page;
+        //    viewModel.CurrentPage = page;
 
-            return this.View(viewModel);
-        }
+        //    return this.View(viewModel);
+        //}
     }
-}
 }
